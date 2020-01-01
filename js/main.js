536d626fdca5ade6bf777ff092d366eb80a4328b
deddyprianto <haicoders12@gmail.com>
2020-01-01 12:11:05 +0700

    menambah file main.js

--- js/main.js
+++ js/main.js
@@ -1,96 +1,99 @@
-;(function () {
-	
+;
+(function () {
+
 	'use strict';
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
-	
-	var fullHeight = function() {
 
-		if ( !isMobile.any() ) {
+	var fullHeight = function () {
+
+		if (!isMobile.any()) {
 			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
+			$(window).resize(function () {
 				$('.js-fullheight').css('height', $(window).height());
 			});
 		}
 	};
 
 	// Parallax
-	var parallax = function() {
+	var parallax = function () {
 		$(window).stellar();
 	};
 
-	var contentWayPoint = function() {
+	var contentWayPoint = function () {
 		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+		$('.animate-box').waypoint(function (direction) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
+			if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
+
 				i++;
 
 				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+				setTimeout(function () {
 
-					$('body .animate-box.item-animate').each(function(k){
+					$('body .animate-box.item-animate').each(function (k) {
 						var el = $(this);
-						setTimeout( function () {
+						setTimeout(function () {
 							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
+							if (effect === 'fadeIn') {
 								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
+							} else if (effect === 'fadeInLeft') {
 								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
+							} else if (effect === 'fadeInRight') {
 								el.addClass('fadeInRight animated-fast');
 							} else {
 								el.addClass('fadeInUp animated-fast');
 							}
 
 							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
+						}, k * 100, 'easeInOutExpo');
 					});
-					
+
 				}, 50);
-				
+
 			}
 
-		} , { offset: '85%' } );
+		}, {
+			offset: '85%'
+		});
 	};
 
 
 
-	var goToTop = function() {
+	var goToTop = function () {
 
-		$('.js-gotop').on('click', function(event){
-			
+		$('.js-gotop').on('click', function (event) {
+
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
-			
+
 			return false;
 		});
 
-		$(window).scroll(function(){
+		$(window).scroll(function () {
 
 			var $win = $(window);
 			if ($win.scrollTop() > 200) {
@@ -100,42 +103,44 @@
 			}
 
 		});
-	
+
 	};
 
-	var pieChart = function() {
+	var pieChart = function () {
 		$('.chart').easyPieChart({
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
 			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
+			trackColor: "#f5f5f5",
 			size: 160,
 			animate: 1000
 		});
 	};
 
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
+	var skillsWayPoint = function () {
+		if ($('#fh5co-skills').length > 0) {
+			$('#fh5co-skills').waypoint(function (direction) {
+
+				if (direction === 'down' && !$(this.element).hasClass('animated')) {
+					setTimeout(pieChart, 400);
 					$(this.element).addClass('animated');
 				}
-			} , { offset: '90%' } );
+			}, {
+				offset: '90%'
+			});
 		}
 
 	};
 
 
 	// Loading page
-	var loaderPage = function() {
+	var loaderPage = function () {
 		$(".fh5co-loader").fadeOut("slow");
 	};
 
-	
-	$(function(){
+
+	$(function () {
 		contentWayPoint();
 		goToTop();
 		loaderPage();
